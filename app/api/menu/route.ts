de849fa06352b67218315c73b612ee15f307d90f
AndrewEl131 <andyelizbarashvilii@gmail.com>
2026-03-24 20:00:13 +0400

    Fix

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -3,9 +3,17 @@ import { connectToDB } from "@/lib/mongoose";
 import menuCategory from "@/models/MenuCategory";
 
 export async function GET() {
-  await connectToDB();
-  const menu = await menuCategory.find();
-  return NextResponse.json(menu);
+  try {
+    await connectToDB();
+    const menu = await menuCategory.find();
+    return NextResponse.json(menu);
+  } catch (err: any) {
+    console.error("API ERROR:", err);
+    return NextResponse.json(
+      { error: err.message },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(req: Request) {
