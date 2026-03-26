68af08dd8fc38dc2d5f425b79de6267099f48f24
AndrewEl131 <andyelizbarashvilii@gmail.com>
2026-03-26 14:45:39 +0400

    Added animations to other pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,21 +2,30 @@ import Footer from "@/components/Footer";
 import React from "react";
 import Image from "next/image";
 import type { Metadata } from "next";
+import Reveal from "@/hooks/useReveal";
 
 export const metadata: Metadata = {
   title: "ჩვენს შესახებ",
   description: "გვერდი ჩვენს შესახებ კაფე STREET#1-ის საიტიზე",
 };
 
-
 export default function about() {
   return (
     <main className="relative">
-      <div className="lg:w-119.25 w-60 lg:h-85 h-50 absolute top-55 place-self-center">
-        <Image src={"/assets/banner.jpg"} fill alt="" className="rounded-xl" />
-      </div>
+      <Reveal>
+        <div className="lg:w-119.25 w-60 lg:h-85 h-50 absolute top-55 place-self-center">
+          <Image
+            src={"/assets/banner.jpg"}
+            fill
+            alt=""
+            className="rounded-xl"
+          />
+        </div>
+      </Reveal>
       <div className="w-full h-75 bg-[#FC530A] flex justify-center items-center">
-        <h1 className="test lg:text-[64px] text-4xl text-amber-50">ჩვენი ამბავი</h1>
+        <h1 className="test lg:text-[64px] text-4xl text-amber-50">
+          ჩვენი ამბავი
+        </h1>
       </div>
       <div className="lg:w-2xl h-auto py-5 mx-auto lg:mt-70 mt-30 lg:px-0 px-5">
         <p>
